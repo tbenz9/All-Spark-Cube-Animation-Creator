1587b718e31e92a321f9b4cb61f266c11e5fee46
Spencer Owen <bubbleboy450@gmail.com>
2012-10-09 20:10:46 -0600

    Fixed thomases silly location error

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <head>
-	<link rel="stylesheet" type="text/css" href="/cube/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
 	
-	<script type='text/javascript' src="/cube/includes/jquery/jquery.js"></script>
-	<script type='text/javascript' src="/cube/includes/processing.js"></script>
-	<script type='text/javascript' src="/cube/functions/javascriptFunctions.js"></script>
-	<script type='text/javascript' src="/cube/3dcube/pre3d.js"></script>
-	<script type='text/javascript' src="/cube/3dcube/pre3d_shape_utils.js"></script>
-	<script type='text/javascript' src="/cube/3dcube/demo_utils.js"></script>
-	<script type='text/javascript' src="/cube/3dcube/colorscube.js"></script>
+	<script type='text/javascript' src="includes/jquery/jquery.js"></script>
+	<script type='text/javascript' src="includes/processing.js"></script>
+	<script type='text/javascript' src="functions/javascriptFunctions.js"></script>
+	<script type='text/javascript' src="3dcube/pre3d.js"></script>
+	<script type='text/javascript' src="3dcube/pre3d_shape_utils.js"></script>
+	<script type='text/javascript' src="3dcube/demo_utils.js"></script>
+	<script type='text/javascript' src="3dcube/colorscube.js"></script>
 </head>
 <body>
 <table width=100% style="border: 2px dotted gray">
